bf0eaecb8196b941fa13b8d9966184dcc30cdf04
Eduardo Douglas <edudouglas@gmail.com>
2018-08-30 18:01:55 -0300

    Removed private bucket

--- temba/utils/s3.py
+++ temba/utils/s3.py
@@ -1,5 +1,4 @@
 from django.core.files.storage import DefaultStorage
-from django.conf import settings
 
 
 class PublicFileStorage(DefaultStorage):
@@ -7,13 +6,5 @@ class PublicFileStorage(DefaultStorage):
     file_overwrite = False
 
 
-class PrivateFileStorage(DefaultStorage):
-    default_acl = "private"
-    file_overwrite = False
-    bucket_name = settings.AWS_STORAGE_PRIVATE_BUCKET_NAME
-
-
 public_file_storage = PublicFileStorage()
 public_file_storage.default_acl = "public-read"
-
-private_file_storage = PrivateFileStorage()
